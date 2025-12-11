49827a7cd494c58ec688c13562c0888a8c1d4620
daveshephard <dshephard@rainierpartners.com>
2025-12-10 17:02:31 -0800

    Readding django

--- src/rainier_apps/settings.py
+++ src/rainier_apps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1%+)1xxd-(r33gw%+a1%fqj@kwlljac=^5k-f3boifgw^n3*5^'
+SECRET_KEY = 'django-insecure-1=2$(41&1(ek1f6(60y_)z)$5_kj&q-makyrjy7n%_!p!56!^y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
